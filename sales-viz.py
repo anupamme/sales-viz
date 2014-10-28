d6231023be79d156088f1be01951225302fe3bdc
Anupam Mediratta <mediratta@Anupams-MacBook-Pro.local>
2014-10-28 13:51:05 +0530

    updated code

--- sales-viz.py
+++ sales-viz.py
@@ -1,12 +1,26 @@
 import cgi
 import webapp2
 
+class Shopify(webapp2.RequestHandler):
+
+    def get(self):
+        html = open('shopify2.html', 'r').read()
+        self.response.headers['Content-Type'] = 'text/html'
+    	self.response.write(html)
+        
+class CSVUpload(webapp2.RequestHandler):
+
+    def get(self):
+        html = open('data5.html', 'r').read()
+        self.response.headers['Content-Type'] = 'text/html'
+    	self.response.write(html)
+        
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
         html = open('draw4.html', 'r').read()
         self.response.headers['Content-Type'] = 'text/html'
     	self.response.write(html)
-        
+
 application = webapp2.WSGIApplication([
-    ('/', MainPage)], debug=True)
+    ('/', MainPage), ('/shopify', Shopify), ('/csvupload', CSVUpload)], debug=True)
